cb0e612cf88aba6f5c99a34b24ffa04302e074f0
Frances D Pasulohan <franpas12123@gmail.com>
2020-02-02 03:32:27 +0800

    added About page

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row>
+    <v-col>
+      <h1>About Page</h1>
+      <p>An app that displays corny dad jokes.</p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best corny joke"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
