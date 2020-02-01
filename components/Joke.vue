1a42672dec92f1a79b521f36ab8b62afb810bf58
Frances D Pasulohan <franpas12123@gmail.com>
2020-02-02 03:34:19 +0800

    Added Joke component

---
+++ components/Joke.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-card>
+        <v-card-subtitle>{{joke}}</v-card-subtitle>
+        <v-card-actions>
+          <v-btn text :to="'/jokes/' + id">See Joke</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Joke",
+  props: ["joke", "id"]
+};
+</script>
+
+<style>
+</style>
